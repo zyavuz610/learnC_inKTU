a82e2660be2f987b3d7ab89763bc2b31b10823ef
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2019-02-07 18:20:45 +0300

    Create 01B_ilk ders.c

---
+++ 0_sinif_ici_kodlar/01B_ilk ders.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+// bu bir yorum satırıdır
+/* bu
+
+da
+
+bir
+
+yorum
+
+bloğudur
+
+*/
+
+int main(void) {
+  printf("Merhaba Programlama Dunyasi\n");
+  printf(".....\n");
+  printf("Bölüme Giriş Yılı:");
+  int buyil = 2019;
+  int yil = 2016;
+  float oran = 2.5;
+  char ch = 'A';
+  int sinif = buyil - yil;
+
+  printf("%d \n%f \n%c \nSınıf:%d",yil,oran,ch,sinif);
+  /*
+  printf("\n");
+  printf("%f",oran);
+  printf("\n");
+  printf("%c",ch);
+  printf("\n");
+  printf("Sınıf:%d",sinif);
+*/
+  // hadi not ortalamasını bulalım
+  float ara_sinav,final_not;
+  ara_sinav = 29.3;
+  final_not = 75.2;
+  float ortalama = 0.5*ara_sinav + 0.5*final_not;
+  printf("Ortalama:%f",ortalama);
+  return 0;
+}
+/* Eve Götürülecek Bilgiler
+- her programda mani() fonk. olması gerekir
+- ekrana bir şey yazmak için printf() fonk. kullanırız
+  - yeni satıra geçmek için: \n
+- her program 2 bileşene sahiptir
+  - veri
+  - veriyi işleyen kod
+- veriler değişkenler içinde saklanır
+- değişkenler farklı veri türleri şeklinde olabilir
+- kabaca veri türleri
+  - tamsayı (int)
+  - reel sayı (float,double)
+  - karakter
+- int yil = 2016;
+  - değişken ismi : yil
+    - değişken isimlendirirken bunun bir kuralı var mı?
+      - dilin ayrılmış kelimelerini kullanamayız, int diye değişken ismi olmaz
+      - büyük küçük harf ayrımı var
+        - yil, Yil, yiL farklı değişkenler
+      - boşluk kullanılmaz, karakter, sayı, _ kullanılabilir
+      - sayı ile başlamaz
+      - aynı değişken bölgesindeki her değişkenin ismi farklı olmalı
+      - hadi kendimize bir tarz (style) seçelim
+        - tamamı küçük harf (tek kelime için)
+        - kelime olarak isim olsun
+        - birden çok kelimeden oluşuyorsa _ ile ayıralım
+        - değişken ismi yaptığı işi anımsatsın
+        - mümkün olduğunca açıklayıcı olsun
+  - değişken değeri : 2016
+  - değişken türü : tamsayı
+  - 1 tane değer içerir
+- printf() ile ekrana değişkenleri yazarken
+  - %d : int türü için
+  - %f : float türü için
+  - %c : char türü için
+*/
