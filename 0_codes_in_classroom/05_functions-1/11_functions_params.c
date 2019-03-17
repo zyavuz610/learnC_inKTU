f4aeb62e0163f1ae7f5ad98e236b7de60a671468
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2019-03-17 12:55:54 +0300

    Create 11_functions_params.c

---
+++ 0_codes_in_classroom/05_functions-1/11_functions_params.c
@@ -0,0 +1,22 @@
+/*************************************************
+  Fonksiyonlar (devam)
+    - Fonksiyon çağırma
+    - Paramtre ve Argüman
+      A parameter is a variable in a method definition. When a method is called, the arguments are the data you pass into the method's parameters.
+    - Call by value
+    - Call by refrence (ayrıntılar pointer gördükten sonra)
+    - Scope rules
+      - global, local, formal değişkenler
+      - aynı isimde global/local/formal değişkenler
+      - değişkenlere ilk değer atama
+    - Fonksiyon tanımlama tipleri
+      - parametreleri pointer ve/veya değişken olan fonk.
+      - geri dönüş değeri pointer veya değişken olan fonk.
+      - const türü parametreler
+*************************************************/
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello World\n");
+  return 0;
+}
