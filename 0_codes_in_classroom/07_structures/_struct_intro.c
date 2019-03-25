cda8327cc9a4cf74d8b417914d6e3ac76be92509
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2019-03-25 13:04:08 +0300

    struct düzeltme

--- 0_codes_in_classroom/07_structures/_struct_intro.c
+++ 0_codes_in_classroom/07_structures/_struct_intro.c
@@ -7,8 +7,9 @@
         - struct türünden değişken tanımlama
         - struct elemanlarına erişim
         - https://www.tutorialspoint.com/cprogramming/c_structures.htm
-    - fonksiyonlara struct değişken geçişi, geri döndürme
+    - fonksiyonlara struct parametre geçişi, geri döndürme
     - struct pointerlar
+    - struct dizileri
     - fonksiyonlara struct pointer geçişi, geri döndürme
     - bit seviyesinde eleman tanımlama
         - https://www.tutorialspoint.com/cprogramming/c_bit_fields.htm
