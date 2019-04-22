865ea65b8255544a59156aa81f576554fd11fc12
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2019-04-22 21:13:50 +0300

    Create Zsort.c

---
+++ 2_useful_c_functions/array/Zsort.c
@@ -0,0 +1,15 @@
+/*
+aşağıda prototipi verilen fonksiyonların içini yazınız ve örnek bir main fonksiyonunda çağırarak bu dosyaya pull-request değişiklik isteğinde bulununuz
+
+*/
+
+void Zsort(int D[],int n);
+void Zsort(float D[],int n);
+void Zsort(double D[],int n);
+void Zsort(char D[]);
+
+// son parametre 1 gelirse artan sırada, -1 gelirse azalan sırada sıralayınız
+void Zsort(int D[],int n,int st);
+void Zsort(float D[],int n,int st);
+void Zsort(double D[],int n,int st);
+void Zsort(char D[],int st);
