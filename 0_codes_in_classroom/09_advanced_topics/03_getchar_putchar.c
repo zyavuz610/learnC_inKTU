3d09edb6419a337dfc78b4d649c5fcce42915ca4
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2019-05-07 18:27:50 +0300

    Create 03_getchar_putchar.c

---
+++ 0_codes_in_classroom/09_advanced_topics/03_getchar_putchar.c
@@ -0,0 +1,22 @@
+/*
+The getchar() and putchar() Functions
+The int getchar(void) function reads the next available character from the screen and returns it as an integer. This function reads only single character at a time. You can use this method in the loop in case you want to read more than one character from the screen.
+
+The int putchar(int c) function puts the passed character on the screen and returns the same character. This function puts only single character at a time. You can use this method in the loop in case you want to display more than one character on the screen. Check the following example
+
+*/
+
+
+#include <stdio.h>
+int main( ) {
+
+   int c;
+
+   printf( "Enter a value :");
+   c = getchar( );
+
+   printf( "\nYou entered: ");
+   putchar( c );
+
+   return 0;
+}
