485a770f46721bc8c230dede366a019c1cec710f
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2021-12-09 12:17:47 +0300

    Update zlib.c

--- 20_sample_codes/zlib.c
+++ 20_sample_codes/zlib.c
@@ -39,6 +39,55 @@ int sumOfArray(int *arr, int n)
     }
     return sum;
 }
+
+int power(int x, int y)
+{
+    int i, p = 1;
+    for (i = 1; i <= y; i++)
+    {
+        p *= x;
+    }
+    return p;
+}
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int strlen(char *str)
+{
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        i++;
+    }
+    return i;
+}
+
+void reverse(char *str)
+{
+    int i, j, len = strlen(str);
+    for (i = 0, j = len - 1; i < j; i++, j--)
+    {
+        swap(&str[i], &str[j]);
+    }
+}
+
+void printArray(int *arr, int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+
+
 int main(){
     int arr[] = {10, 20, 30, 40, 50};
     int n = sizeof(arr) / sizeof(arr[0]);
